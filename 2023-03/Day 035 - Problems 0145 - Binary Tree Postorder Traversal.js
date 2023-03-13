2bf90146e74843825d06f959583b76e58cb26943
wilson <p780729@hotmail.com>
2023-03-13 22:07:37 +0800

    Day 035 - Problems 0145 - Binary Tree Postorder Traversal

---
+++ 2023-03/Day 035 - Problems 0145 - Binary Tree Postorder Traversal.js
@@ -0,0 +1,53 @@
+/*
+    145. Binary Tree Postorder Traversal
+    Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+    
+    Example 1:
+    Input: root = [1,null,2,3]
+    Output: [3,2,1]
+
+    Example 2:
+    Input: root = []
+    Output: []
+
+    Example 3:
+    Input: root = [1]
+    Output: [1]
+
+    Constraints:
+
+    The number of the nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+
+    Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    if (!root) return [];
+
+    const stack = [root];
+    const result = [];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        result.unshift(node.val);
+
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+
+    return result;
+};
